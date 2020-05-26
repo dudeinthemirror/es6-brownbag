5a4e62ba1dec2b7ef90197dfee50d89025979846
dudeinthemirror <dude@lukie.io>
2020-05-26 08:58:46 -0700

    Updating optionalChaining

--- src/6_misc/optionalChaining.js
+++ src/6_misc/optionalChaining.js
@@ -10,7 +10,36 @@ const obj = {
   },
 };
 
-// console.log(obj.e.h.t);
+// nested prop
+// console.log(obj.e.h.t); // <-- cannot read prop 't' of undefined
 
 // console.log(obj && obj.e && obj.e.h && obj.e.h.t);
-console.log(obj.e?.h?.t);
+// console.log(obj.e?.h?.t);
+
+// console.log(obj?.['prop' + 'number']);
+
+// array
+// const arr = [1, 2, 3, 4];
+// let arrItem = arr?.[8];
+// console.log(arrItem);
+
+// function
+// const someFunc = (onSuccess, onError) => {
+//   try {
+//     if (asdf === 1) {
+//       onSuccess();
+//     }
+//   } catch (error) {
+//     onError?.(error.message);
+//   }
+// };
+
+// someFunc(
+//   () => console.log('SUCCESS'),
+//   (msg) => console.log('ERROR: ', msg)
+// );
+
+// someFunc(() => console.log('SUCCESS'));
+
+// caveat
+// obj.e?.h?.t = 1; // <-- invalid left hand side in assignment
